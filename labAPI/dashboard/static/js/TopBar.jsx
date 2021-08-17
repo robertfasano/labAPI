be986994a125531ed8ecaa94769ff68ccc696ffe
Robert Fasano <robert.j.fasano@colorado.edu>
2021-08-17 14:59:52 -0600

    Remove unused imports

--- labAPI/dashboard/static/js/TopBar.jsx
+++ labAPI/dashboard/static/js/TopBar.jsx
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Drawer from "@material-ui/core/Drawer";
-import { makeStyles } from '@material-ui/core/styles';
-import MenuOpenIcon from '@material-ui/icons/MenuOpen';
-import ClearIcon from '@material-ui/icons/Clear';
-import SpeedIcon from '@material-ui/icons/Speed';
-import TimelineIcon from '@material-ui/icons/Timeline';
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default function ButtonAppBar(props) {
+export default function ButtonAppBar() {
   const useStyles = makeStyles(theme => ({
     appBar: {
       zIndex: theme.zIndex.drawer + 1,
       '&:focus': {outline: 'none'},
     },
-
     button: {    '&:focus': {outline: 'none'},},
     appBarSpacer: theme.mixins.toolbar
 
