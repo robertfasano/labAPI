ab640d1aca0cc55ad0b84cfff9ca54b57a287329
Robert Fasano <robert.j.fasano@colorado.edu>
2021-08-17 14:49:41 -0600

    Add background gradient

--- labAPI/dashboard/static/js/App.jsx
+++ labAPI/dashboard/static/js/App.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from 'react'
 import ButtonAppBar from './TopBar.jsx'
 import NetworkDisplay from './parameters/NetworkDisplay.jsx'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import { SnackbarProvider } from 'notistack';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { SnackbarProvider } from 'notistack'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 const theme = createMuiTheme({
   palette: {
     primary: {main: "#67001a"},
-    secondary: {main: '#004e67'}
+    secondary: {main: '#004e67'},
   },
-});
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          background: 'linear-gradient(180deg, #e6e6e6 20%, #ffffff 90%)',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed'
+        }
+      }
+    }
+  }
+
+
+})
 
 export default function App(props){
   return (
-    <div>
-      <SnackbarProvider maxSnack={3} preventDuplicate anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+    <SnackbarProvider maxSnack={3} preventDuplicate anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
       <ThemeProvider theme={theme}>
-      <ButtonAppBar/>
-      <Box m={4}>
-      <Grid container spacing={2} justify='flex-start'>
-        <Grid item>
-          <NetworkDisplay snapshot={props.snapshot}/>
-        </Grid>
-      </Grid>
-      </Box>
+        <CssBaseline/>
+        <ButtonAppBar/>
+        <NetworkDisplay/>
       </ThemeProvider>
     </SnackbarProvider>
-    </div>
   )
 }
