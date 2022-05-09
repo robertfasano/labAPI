6d1fb144ddca0674db5d74048d3860bf70e80f2d
Robert Fasano <robert.j.fasano@colorado.edu>
2022-05-09 14:42:19 -0600

    Refactor parameter type cases

--- labAPI/dashboard/static/js/parameters/ParameterDisplay.jsx
+++ labAPI/dashboard/static/js/parameters/ParameterDisplay.jsx
@@ -18,8 +18,128 @@ import ToggleOnIcon from '@mui/icons-material/ToggleOn'
 import ListIcon from '@mui/icons-material/List'
 import CodeIcon from '@mui/icons-material/Code'
 
+const rowHeight = '50px'
+
+function Icon (type) {
+  const props = {size: 20, sx: {color: '#8f8f8f'}}
+
+  switch(type) {
+    case 'switch':
+      return <ToggleOnIcon {...props}/>
+    case 'selector':
+      return <ListIcon {...props}/>
+    case 'knob':
+      return <TuneIcon {...props}/>
+    case 'function':
+      return <CodeIcon {...props}/>
+    case 'measurement':
+      return <ScienceIcon {...props}/>
+    default:
+      return null
+  }
+}
+
+
+function SwitchWidget (props) {
+  function handleToggle(event) {
+    event.stopPropagation()
+    const newState = Boolean(1 - props.data.value)
+    props.dispatch({type: 'parameters/update', path: props.path, value: newState})
+    post('/parameters/' + props.path, {'value': newState})
+  }
+  return (
+    <Switch
+      checked={props.data.value}
+      onChange={(event) => handleToggle(event)}
+    />
+  )
+}
+
+function SelectWidget (props) {
+  function handleChange(event) {
+    event.stopPropagation()
+    const value = event.target.value
+    props.dispatch({type: 'parameters/update', path: props.path, value: value})
+    post('/parameters/' + props.path, {'value': value})
+  }
+
+  return (
+    <Select value={props.data.value}
+    style={{width: 80}}
+    onChange={(event) => handleChange(event)}
+    variant='standard'
+    >
+      {props.data.options.map((row, index) => {
+        return (
+          <MenuItem key={row} value={row}>{row}</MenuItem>
+          )
+        }
+      )
+      }
+    </Select>
+  )
+}
+
+function KnobWidget (props) {
+  function send(event) {
+    const value = parseFloat(event.target.value)
+    post('/parameters/' + props.path, {'value': value})
+    props.dispatch({type: 'parameters/update', path: props.path, value: value})
+  }
+
+  return (
+    <ValidatedInput placeholder={parseFloat((Number(props.data.value).toFixed(3)))}
+      min={props.data.min}
+      max={props.data.max}
+      onKeyPress={send}
+    />
+  )
+}
+
+function FunctionWidget (props) {
+  function send() {
+    get('/functions/' + props.path)
+  }
+  return (
+    <IconButton onClick={send}>
+      <PlayArrowIcon/>
+    </IconButton>
+  )
+}
+
+function MeasurementWidget (props) {
+  let value = ''
+  if (props.value != null) {
+    value = props.value.toFixed(3)
+  }
+
+  return (
+    <TextField value={value}
+    InputProps={{style: {color: props.alert? '#FF0000': "rgba(0, 0, 0, 0.38)"}}}
+    error={props.alert}
+    style={{width: 100}}
+    variant='standard'
+  />
+  )
+}
+function Widget (type, props) {
+  switch(type) {
+    case 'switch':
+      return <SwitchWidget {...props}/>
+    case 'selector':
+      return <SelectWidget {...props}/>
+    case 'knob':
+      return <KnobWidget {...props}/>
+    case 'function':
+      return <FunctionWidget {...props}/>
+    case 'measurement':
+      return <MeasurementWidget {...props}/>
+    default:
+      return null
+  }
+}
+
 function ParameterRow(props) {
-  const rowHeight = '50px'
   const [alertKey, setAlertKey] = React.useState('')
   React.useEffect(() => {
     if (props.alert) {
@@ -36,174 +156,26 @@ function ParameterRow(props) {
   if (!props.isVisible) {
     return null
   }
-  if (props.data.type == 'switch') {
-    function handleToggle(event) {
-      event.stopPropagation()
-      const newState = Boolean(1 - props.data.value)
-      props.dispatch({type: 'parameters/update', path: props.path, value: newState})
-      post('/parameters/' + props.path, {'value': newState})
-    }
-    return (
-      <Box pt='8px' borderTop={0.1} borderColor='#b5b5b5' key={props.path} height={rowHeight}>
-        <Grid container spacing={1} alignItems="center" >
-          <Grid container item xs={2}>
-            <Box ml={2}>
-              <ToggleOnIcon size={20} sx={{color:'#8f8f8f'}}/>
-            </Box>
-          </Grid>
 
-          <Grid container item xs={8} justifyContent='flex-start'>
-            <Typography>{props.name}</Typography>
-          </Grid>
-
-          <Grid container item xs={2} justifyContent='center'>
-            <Switch
-              checked={props.data.value}
-              onChange={(event) => handleToggle(event)}
-            />
-          </Grid>
-        </Grid>
-      </Box>
-
-    )
-  }
-
-
- if (props.data.type == 'selector') {
-    function handleChange(event) {
-      event.stopPropagation()
-      const value = event.target.value
-      props.dispatch({type: 'parameters/update', path: props.path, value: value})
-      post('/parameters/' + props.path, {'value': value})
-    }
-
-    return (
-
-      <Box pt='8px' borderTop={0.1} borderColor='#b5b5b5' key={props.path} height={rowHeight}>
-      <Grid container spacing={1} alignItems="center">
+  return (
+    <Box pt='8px' borderTop={0.1} borderColor='#b5b5b5' key={props.path} height={rowHeight}>
+      <Grid container spacing={1} alignItems="center" >
         <Grid container item xs={2}>
           <Box ml={2}>
-            <ListIcon size={20} sx={{color:'#8f8f8f'}}/>
+            {Icon(props.data.type)}
           </Box>
         </Grid>
 
-        <Grid container item xs={6} justifyContent='flex-start'>
+        <Grid container item xs={8} justifyContent='flex-start'>
           <Typography>{props.name}</Typography>
         </Grid>
 
-        <Grid container item xs={4} justifyContent='flex-end'>
-          <Select value={props.data.value}
-                  style={{width: 80}}
-                  onChange={(event) => handleChange(event)}
-                  variant='standard'
-                  >
-            {props.data.options.map((row, index) => {
-                return (
-                  <MenuItem key={row} value={row}>{row}</MenuItem>
-                  )
-                }
-              )
-            }
-          </Select>
+        <Grid container item xs={2} justifyContent='center'>
+          {Widget(props.data.type, props)}
         </Grid>
       </Grid>
-      </Box>
-    )
-  }
-
-  if (props.data.type == 'knob') {
-    function send(event) {
-      const value = parseFloat(event.target.value)
-      post('/parameters/' + props.path, {'value': value})
-      props.dispatch({type: 'parameters/update', path: props.path, value: value})
-    }
-
-    return (
-      <Box pt='8px' borderTop={0.1} borderColor='#b5b5b5' key={props.path} height={rowHeight}>
-      <Grid container spacing={1} alignItems="center">
-        <Grid container item xs={2}>
-          <Box ml={2}>
-            <TuneIcon size={20} sx={{color:'#8f8f8f'}}/>
-          </Box>
-        </Grid>
-
-        <Grid container item xs={6} justifyContent='flex-start'>
-          <Typography>{props.name}</Typography>
-        </Grid>
-
-        <Grid container item xs={4} justifyContent='flex-end'>
-          <ValidatedInput placeholder={parseFloat((Number(props.data.value).toFixed(3)))}
-                          min={props.data.min}
-                          max={props.data.max}
-                          onKeyPress={send}
-          />
-        </Grid>
-      </Grid>
-      </Box>
-    )
-  }
-
-  if (props.data.type == 'function') {
-    function send() {
-      get('/functions/' + props.path)
-    }
-
-    return (
-      <Box pt='8px' borderTop={0.1} borderColor='#b5b5b5' key={props.path} height={rowHeight}>
-        <Grid container spacing={1} alignItems="center">
-          <Grid container item xs={2}>
-            <Box ml={2}>
-              <CodeIcon size={20} sx={{color:'#8f8f8f'}}/>
-            </Box>
-          </Grid>
-
-          <Grid container item xs={6} justifyContent='flex-start'>
-            <Typography>{props.name}</Typography>
-          </Grid>
-
-          <Grid container item xs={4} justifyContent='flex-end'>
-            <IconButton onClick={send}>
-              <PlayArrowIcon/>
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Box>
-    )
-  }
-
-  if (props.data.type == 'measurement') {
-    let value = ''
-    if (props.value != null) {
-      value = props.value.toFixed(3)
-    }
-
-    return (
-      <Box pt='8px' borderTop={0.1} borderColor='#b5b5b5' key={props.path} height={rowHeight}>
-        <Grid container spacing={1} alignItems="center">
-          <Grid container item xs={2}>
-            <Box ml={2}>
-              <ScienceIcon size={20} sx={{color:'#8f8f8f'}}/>
-            </Box>
-          </Grid>
-
-          <Grid container item xs={6} justifyContent='flex-start'>
-            <Typography>{props.name}</Typography>
-          </Grid>
-
-          <Grid container item xs={4} justifyContent='flex-end'>
-            <TextField value={value}
-                      InputProps={{style: {color: props.alert? '#FF0000': "rgba(0, 0, 0, 0.38)"}}}
-                      error={props.alert}
-                      style={{width: 100}}
-                      variant='standard'
-            />
-            {/* <TextField value={value}/> */}
-          </Grid>
-        </Grid>
-      </Box>
-    )
-  }
-
+    </Box>
+  )
 }
 
 function mapStateToProps(state, ownProps){
@@ -217,4 +189,3 @@ function mapStateToProps(state, ownProps){
 }
 
 export default withSnackbar(connect(mapStateToProps)(ParameterRow))
-// export default connect(mapStateToProps)(ParameterRow)
