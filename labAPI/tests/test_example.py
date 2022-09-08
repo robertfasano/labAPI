d969383e83d338803f1dfc9117d672ddec762bb0
Fasano <robert.j.fasano@colorado.edu>
2022-09-08 13:12:31 -0600

    Set up trivial build testing

--- labAPI/tests/test_example.py
+++ labAPI/tests/test_example.py
@@ -5,7 +5,9 @@ import numpy as np
 def test_example(test=True, host=False):
     x = Knob('x', 0)
     y = Measurement('y', get_cmd = lambda: x.get()**2*(1+np.random.normal(0, 0.01))+np.random.normal(0, 0.01), monitor=True)
-    env = Environment()
-    env.monitor.resume()
     if host:
+        env = Environment()
+        env.monitor.resume()
         env.host('127.0.0.1:9010')
+
+        
